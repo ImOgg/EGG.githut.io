00755cec7e595791d54e8dcd842f1a442ce2ead6
ImOgg <106737132+ImOgg@users.noreply.github.com>
2022-09-05 00:52:45 +0800

    v2
    google form

--- EGG/js/nav.js
+++ EGG/js/nav.js
@@ -4,3 +4,30 @@ $('.btn-menu').click(function () {
     // alert(11111);
 })
 
+
+function GoForm() { //這裡要對應到自己的 javascript 名稱
+    //宣告欄位
+    var field1 = $("[name='name']").val();
+    var field2 = $("[name='tel']").val();
+    var field3 = $("[name='email']").val();
+    var field4 = $("[name='text']").val();
+    $.ajax({
+        url: "https://docs.google.com/forms/u/0/d/e/1FAIpQLSdcyelVxuhgMA3oQjn1EMT-Y2AexpSgVE35lKgChkWBggauVA/formResponse", //Google Form 裡面的 form action 網址 ＊＊記得要填＊＊
+        data: { //Google Form 裡面的欄位 name ＊＊記得要改＊＊
+            "entry.1603205191":field1,
+            "entry.1310954240":field2,
+            "entry.1537638940":field3,
+            "entry.1996598147":field4,
+        },
+        type: "POST",
+        dataType: "xml",
+        statusCode: {
+            0: function () {
+                alert("已送出!"); //完成送出表單的警告視窗
+            },
+            200: function () {
+                alert("已送出!"); //完成送出表單的警告視窗
+            }
+        }
+    });
+}
